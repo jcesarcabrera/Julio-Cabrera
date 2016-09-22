6b89a42d3a846db2c7bc1ffefc044714efea689a
Julio Cabrera <jcabrera@rbsas.co>
2016-09-22 13:40:41 -0500

    Se carga manual de generación de contingencias para usos en la ruta: C:\WorkSpaceRB\Manuales

--- DaviviendaHCS/Scripts/ContingenciaUsos.bat
+++ DaviviendaHCS/Scripts/ContingenciaUsos.bat
@@ -1,9 +1,10 @@
 @ECHO OFF
 ECHO SE ENVIARA EL MENSAJE PARA GENERAR EL ARCHIVO DE CONTINGENCIA DAVIVIENDA
-SET /p queuecontingencia=<C:\WorkSpaceRB\DaviviendaHCS\Scripts\QueueContingencia.txt
-SET /p jarlocation=<C:\WorkSpaceRB\DaviviendaHCS\Scripts\jarlocation.txt
-SET /p ems_server=<C:\WorkSpaceRB\DaviviendaHCS\Scripts\EMS_SERVER.txt
-SET /p contingenciapath=<C:\WorkSpaceRB\DaviviendaHCS\Scripts\ContingenciaPathPrefix.txt
+SET /p archivos=<C:\WorkSpaceRB\DaviviendaHCS\Scripts\archivos.txt
+SET /p queuecontingencia=<%archivos%QueueContingencia.txt
+SET /p jarlocation=<%archivos%jarlocation.txt
+SET /p ems_server=<%archivos%EMS_SERVER.txt
+SET /p contingenciapath=<%archivos%ContingenciaPathPrefix.txt
 SET "EMSUser=admin"
 SET /P message=Por favor indique la fecha a realizar la contingencia: 
 java -jar %jarlocation% -server %ems_server% -user %EMSUser% -queue %queuecontingencia% %message%
